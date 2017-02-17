17d2ba8d12f1db3823d5b1dde6ef0755e03d4fbc
Victor Yap <victoryap@Victors-MacBook-Air.local>
2017-02-17 15:38:30 +0800

    Added copy

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -2,7 +2,11 @@ var React = require('react');
 
 var Example = (props) => {
   return (
-    <h3>Example component!</h3>
+    <div>
+      <h3>Example component!</h3>
+      <p>Welcome to Example page</p>
+    </div>
+
   );
 }
 
