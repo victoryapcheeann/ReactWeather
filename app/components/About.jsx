8688c776ed787ca4b2761a6fda84c854dc27db5f
Victor Yap <victoryap@Victors-MacBook-Air.local>
2017-02-24 12:31:10 +0800

    Update about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -16,6 +16,14 @@ var About = (props) => {
           <a href = "http://openweathermap.org">Open Weather Map</a> -
             I used Open Weather MAP API to search for weather data by city name.
         </li>
+        <li>
+          <a href = "http://foundation.zurb.com/">Foundation</a> -
+            Styling and Design
+        </li>
+        <li>
+          <a href = "http://sass-lang.com/">SCSS</a> -
+             Allows variables to be defined in CSS
+        </li>
       </ul>
     </div>
   );
