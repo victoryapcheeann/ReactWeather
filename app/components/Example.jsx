17a0eeeb9e1c2a3edeb95bb533bc4c4ce8ce4843
Victor Yap <victoryap@Victors-MacBook-Air.local>
2017-02-24 12:24:00 +0800

    Add nav search support and example link

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -4,17 +4,17 @@ var {Link} = require('react-router');
 var Example = (props) => {
   return (
     <div>
-      <h1 className ="text-center page-title">Examples!</h1>
-      <p>Here are some example to try out the app!</p>
+      <h1 className ="text-center page-title">Example</h1>
+      <p>Here are some examples to try out the app!</p>
       <ol>
         <li>
-          <Link to = "/?location = Singapore">Singapore, SG</Link>
+          <Link to = "/?location=Singapore">Singapore, SG</Link>
         </li>
         <li>
-          <Link to = "/?location = Malaysia">Malaysia, MY</Link>
+          <Link to = "/?location=Malaysia">Malaysia, MY</Link>
         </li>
         <li>
-          <Link to = "/?location = China">China, CN</Link>
+          <Link to = "/?location=China">China, CN</Link>
         </li>
       </ol>
     </div>
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,7 +4,14 @@ var {Link, IndexLink} = require('react-router');
 var Nav = React.createClass({
   onSearch: function(e) {
     e.preventDefault();
-    alert('Not yet ready!');
+
+    var location = this.refs.search.value;
+    var encodedLocation = encodeURIComponent(location);
+
+    if (location.length > 0) {
+      this.refs.search.value = '';
+      window.location.hash = '#/?location=' + encodedLocation;
+    }
   },
 
   render:function () {
@@ -28,7 +35,7 @@ var Nav = React.createClass({
         <form onSubmit = {this.onSearch}>
           <ul className = "menu">
             <li>
-              <input type = "search" placeholder = "Search Weather"/>
+              <input type = "search" placeholder = "Search Weather" ref = "search"/>
             </li>
             <li>
               <input type = "submit" className= "button" value = "Get Weather"/>
